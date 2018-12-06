803406cc685bd2ce670e3cf28379d0838c05553c
LemonB-JW <judywong0731@gmail.com>
2018-12-06 02:06:06 -0500

    Register

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Register } from './Register';
+
+
+export class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Register/>
+            </div>
+        );
+    }
+}
