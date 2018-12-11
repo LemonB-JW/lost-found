4bf3059a640a8b6a35b944b1e49ea377e5c7f749
LemonB-JW <judywong0731@gmail.com>
2018-12-11 16:41:08 -0500

    send load post request

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import { GEO_OPTIONS } from '../constants';
-import { Tabs, Button } from 'antd';
+import { Tabs, Button, Spin } from 'antd';
+import { API_ROOT } from '../constants'
+import { AUTH_PREFIX } from '../constants';
 
 const TabPane = Tabs.TabPane;
 
@@ -30,10 +33,34 @@ export class Home extends Component {
         }
     }
 
+    // Load posts whose geo information is around you
+    loadNearbyPost = () => {
+        // get geolocation key from localStorage
+        const {lat, lon} = JSON.parse(localStorage.getItem('POS_KEY'));
+        $.ajax({
+          url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=200`,
+          method: 'GET',
+          headers: {
+            Authorization: `${AUTH_PREFIX} ${localStorage.getItem('TOKEN_KEY')}`,
+          }
+        }).then((response) => {
+            console.log(response);
+        }, (err) => {
+            console.log(err.responseText);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
     onSuccessLoad = (pos) => {
         // stop showing "loading"
         this.setState({ loadingGeoLocation: false});
+        // get exact coordinates;
+        const {latitude, longitude} = pos.coords;
+        localStorage.setItem('POS_KEY', JSON.stringify({lat: latitude, lon: longitude}));
         console.log(pos);
+        this.loadNearbyPost();
+
     }
 
     onFailedLoad = (err) => {
@@ -46,10 +73,16 @@ export class Home extends Component {
         return (
             <div className="tabs">
               <Tabs tabBarExtraContent={operations}>
-                <TabPane tab="Post" key="1">{this.state.loadingGeoLocation ? "loading" : "not loading"}</TabPane>
+                <TabPane
+                  tab="Post" key="1">{
+                      this.state.loadingGeoLocation ?
+                        <Spin tip="Loading Geolocation..."/>
+                          : "not loading"}
+                  </TabPane>
                 <TabPane tab="Map" key="2">Content of tab 2</TabPane>
               </Tabs>
             </div>
+
         )
     };
 
