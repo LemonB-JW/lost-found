2caef15eb52ea5261aef79ba175e3ce1c183ebd4
LemonB-JW <judywong0731@gmail.com>
2018-12-11 14:03:41 -0500

    geolocation init

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
-
+import { GEO_OPTIONS } from '../constants';
 
 export class Home extends Component {
+    componentDidMount() {
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = ()=> {
+        // check if browser support "geolocation"
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+              this.onSuccessLoad,
+              this.onFailedLoad,
+              GEO_OPTIONS
+            );
+        } else {
+
+        }
+    }
+
+    onSuccessLoad = (pos) => {
+        console.log(pos);
+    }
+
+    onFailedLoad = (err) => {
+        console.log(err);
+    }
     render() {
         return (
             <div>
