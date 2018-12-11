be0e3ac16000cec266240bc8ebdd30f7e9c2b1cc
LemonB-JW <judywong0731@gmail.com>
2018-12-11 15:45:33 -0500

    loading state

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,14 @@ const TabPane = Tabs.TabPane;
 
 
 export class Home extends Component {
+    //
+    state = {
+        // check if it is loading geolocation
+        loadingGeoLocation: false
+    }
+
     componentDidMount() {
+        this.setState({ loadingGeoLocation: true});
         this.getGeoLocation();
     }
 
@@ -24,22 +31,26 @@ export class Home extends Component {
     }
 
     onSuccessLoad = (pos) => {
+        // stop showing "loading"
+        this.setState({ loadingGeoLocation: false});
         console.log(pos);
     }
 
     onFailedLoad = (err) => {
+        // stop showing "loading"
+        this.setState({ loadingGeoLocation: true});
         console.log(err);
     }
     render() {
-      const operations = <Button type="primary" icon="upload">I Want to Post!</Button>;
-      return (
+        const operations = <Button type="primary" icon="upload">I Want to Post!</Button>;
+        return (
             <div className="tabs">
               <Tabs tabBarExtraContent={operations}>
-                <TabPane tab="Post" key="1">Content of tab 1</TabPane>
+                <TabPane tab="Post" key="1">{this.state.loadingGeoLocation ? "loading" : "not loading"}</TabPane>
                 <TabPane tab="Map" key="2">Content of tab 2</TabPane>
               </Tabs>
             </div>
         )
-    }
+    };
 
 }
