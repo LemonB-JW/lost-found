0f05d1e27701264a54fac73dfe1b715de297c950
LemonB-JW <judywong0731@gmail.com>
2018-12-12 09:11:13 -0500

    fix googlemap api

--- src/components/Home.js
+++ src/components/Home.js
@@ -142,7 +142,7 @@ export class Home extends Component {
                   </TabPane>
                 <TabPane tab="Map" key="2">
                   <WrappedMap
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJMryJTq6ooc-0lsIN399CIUWBze2nrNA&v=3.exp&libraries=geometry,drawing,places"
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=youkey&v=3.exp&libraries=geometry,drawing,places"
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `400px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
