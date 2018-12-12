0dc1e9e764d3330479e4188852f3d38770913666
LemonB-JW <judywong0731@gmail.com>
2018-12-11 22:34:43 -0500

    Finish Gallery.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { GEO_OPTIONS } from '../constants';
 import { Tabs, Button, Spin } from 'antd';
 import { API_ROOT } from '../constants'
 import { AUTH_PREFIX } from '../constants';
+import {Gallery} from './Gallery'
 
 const TabPane = Tabs.TabPane;
 
@@ -14,16 +15,17 @@ export class Home extends Component {
         // check if it is loading geolocation
         loadingGeoLocation: false,
         loadingPosts: false,
-        error: ''
+        error: '',
+        posts: []
     }
 
     componentDidMount() {
-        this.setState({ loadingGeoLocation: true, error: ''});
         this.getGeoLocation();
     }
 
     getGeoLocation = ()=> {
-        // check if browser support "geolocation"
+        this.setState({ loadingGeoLocation: true, error: ''});
+      // check if browser support "geolocation"
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(
               this.onSuccessLoad,
@@ -60,23 +62,24 @@ export class Home extends Component {
         this.setState({ loadingPosts: true, error: ''});
         // get geolocation key from localStorage
         const {lat, lon} = JSON.parse(localStorage.getItem('POS_KEY'));
-      $.ajax({
-        url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=200`,
-        method: 'GET',
-        headers: {
+        $.ajax({
+            url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=200`,
+            method: 'GET',
+            headers: {
             Authorization: `${AUTH_PREFIX} ${localStorage.getItem('TOKEN_KEY')}`,
          }
         }).then((response) => {
-            console.log(response)
-            this.setState({ loadingPosts: false });
-      }, (err) => {
+            console.log(response);
+            this.setState({ posts: response, loadingPosts: false, error: ''});
+            }, (err) => {
             console.log(err.responseText);
             this.setState({ loadingPosts: false });
             this.setState({ error: 'Sorry, loading posts failed!'});
-      }).catch((err) => {
+        }).catch((err) => {
             console.log(err);
         });
     }
+
 
     getGalleryPanelContent = () => {
         if (this.state.error) {
@@ -85,9 +88,41 @@ export class Home extends Component {
             return  <Spin tip="Loading Geolocation..."/>;
         } else if (this.state.loadingPosts) {
             return  <Spin tip="Loading Posts..."/>;
-
+        } else if (this.state.post && this.state.post.length >0) {
+            // map info in posts to images info
+            const images = this.state.post.map((post) => {
+              return {
+                user: post.user,
+                src: post.url,
+                thumbnail: post.url,
+                caption: post.message,
+                thumbnailWidth: 400,
+                thumbnailHeight: 300
+              };
+            });
+            return <Gallery images={images}/>;
         } else {
+          const temp = [
+            {
+              src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
+              thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
+              thumbnailWidth: 271,
+              thumbnailHeight: 320,
+              tags: [{value: "Nature", title: "Nature | Flowers"}],
+              caption: "Orange Macro (Tom Eversley - isorepublic.com)"
+            },
+            {
+              src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
+              thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
+              thumbnailWidth: 320,
+              thumbnailHeight: 190,
+              tags: [{value: "Architecture", title: "Architecture | Outdoors"},
+                {value: "Industrial", title: "Industrial"}],
+              caption: "286H (gratisography.com)"
+            }
+          ];
 
+          return <Gallery images={temp}/>;
         }
     }
 
@@ -95,7 +130,7 @@ export class Home extends Component {
     render() {
         const operations = <Button type="primary" icon="upload">I Want to Post!</Button>;
         return (
-            <div className="tabs">
+            <div className="main-tabs">
               <Tabs tabBarExtraContent={operations}>
                 <TabPane
                   tab="Post" key="1">
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,3 @@
-import {Alert, Spin} from 'antd/lib/index'
-
 export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
 export const GEO_OPTIONS = {
   enableHighAccuracy:true,
